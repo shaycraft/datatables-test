9eea1a55ffbca0c97d8e6e1437d9b0f1335dec2b
Sam <phreakdawg@gmail.com>
2015-06-27 16:32:50 -0600

    gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var flatten = require('gulp-flatten');
+var concat = require('gulp-concat');
 
 gulp.task('bower', function() {
 	return gulp.src('bower_components/**/*.min.js')
@@ -7,4 +8,16 @@ gulp.task('bower', function() {
 		.pipe(gulp.dest('public/app/assets/js/vendor/'));
 });
 
-gulp.task('default', ['bower']);
+//Add bower dependencies in here.  Order matters.
+gulp.task('vendor', function() {
+    gulp.src([
+            'public/app/assets/js/vendor/angular.min.js',
+            'public/app/assets/js/vendor/jquery.min.js',
+            'public/app/assets/js/vendor/jquery.dataTables.min.js',
+            'public/app/assets/js/vendor/sizzle.min.js'
+        ])
+        .pipe(concat('vendor.min.js'))
+        .pipe(gulp.dest('public/app/dist/'));
+});
+
+gulp.task('default', ['bower', 'vendor']);
