010d7db068ad59ea4200887c48b32f2cd171de9a
Sam <phreakdawg@gmail.com>
2015-06-28 00:07:29 -0600

    Adds bootstrap them and datatables

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,31 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
 gulp.task('bower', function() {
-	return gulp.src('bower_components/**/*.min.js')
+	gulp.src('bower_components/**/*.min.js')
 		.pipe(flatten())
 		.pipe(gulp.dest('public/app/assets/js/vendor/'));
+    gulp.src('bower_components/bootstrip/dist/js/bootstrap.js')
+        .pipe(flatten())
+        .pipe(gulp.dest('public/app/assets/js/vendor/'));
 });
 
 //Add bower dependencies in here.  Order matters.
 gulp.task('vendor', function() {
     gulp.src([
-            'public/app/assets/js/vendor/angular.min.js',
-            'public/app/assets/js/vendor/jquery.min.js',
-            'public/app/assets/js/vendor/jquery.dataTables.min.js',
-            'public/app/assets/js/vendor/sizzle.min.js'
-        ])
-        .pipe(concat('vendor.min.js'))
-        .pipe(gulp.dest('public/app/dist/'));
+        'public/app/assets/js/vendor/angular.min.js',
+        'public/app/assets/js/vendor/jquery.min.js',
+        'public/app/assets/js/vendor/bootstrap.js',
+        'public/app/assets/js/vendor/jquery.dataTables.min.js',
+        'public/app/assets/js/vendor/sizzle.min.js'
+    ])
+    .pipe(concat('vendor.min.js'))
+    .pipe(gulp.dest('public/app/dist/'));
+});
+
+gulp.task('css', function() {
+    return gulp.src('bower_components/bootstrap/dist/css/bootstrap.css')
+        .pipe(concat('site.css'))
+        .pipe(gulp.dest('public/app/dist'));
 });
 
 gulp.task('scripts', function() {
@@ -28,4 +38,8 @@ gulp.task('scripts', function() {
         .pipe(rename('datatables-test.min.js'))
 });
 
-gulp.task('default', ['bower', 'vendor', 'scripts']);
+gulp.task('watch', function() {
+    gulp.watch('public/app/scripts/**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['bower', 'vendor', 'scripts', 'css', 'watch']);
